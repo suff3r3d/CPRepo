779e11b7c838fd90be963fa954eac148ef5acf0d
= <hgchung.dinh@gmail.com>
2024-11-15 12:25:45 +0700

    Create script to generate notebook

---
+++ gen-notebook-script.sh
@@ -0,0 +1,10 @@
+# I only want to compile code, not math notes
+
+cp -r ./data-structures/ ./notebook-include
+cp -r ./graphs/ ./notebook-include
+cp -r ./mathematics/ ./notebook-include
+cp -r ./string/ ./notebook-include
+
+cd notebook-include
+notebook-generator -c 3 -a "chungdinh, distiled, 5onyy" .
+cd ..
