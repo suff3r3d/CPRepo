24305e9de3d90007624a41da3422c2a5f83ee614
Chung Dinh Hoang <hgchung.dinh@gmail.com>
2024-11-16 16:46:49 +0700

    edit gen script

--- gen-notebook-script.sh
+++ gen-notebook-script.sh
@@ -4,6 +4,8 @@ cp -r ./data-structures/ ./notebook-include
 cp -r ./graphs/ ./notebook-include
 cp -r ./mathematics/ ./notebook-include
 cp -r ./string/ ./notebook-include
+cp gen.cpp ./notebook-include
+cp rng.cpp ./notebook-include
 
 cd notebook-include
 notebook-generator -c 3 -a "chungdinh, distiled, 5onyy" .
