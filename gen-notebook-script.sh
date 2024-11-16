e83b021e02592e4ed671aca6e126eadfb7eef1be
Chung Dinh Hoang <hgchung.dinh@gmail.com>
2024-11-16 17:37:22 +0700

    update gen script

--- gen-notebook-script.sh
+++ gen-notebook-script.sh
@@ -1,5 +1,7 @@
 # I only want to compile code, not math notes
 
+rm -r ./notebook-include/*
+
 cp -r ./data-structures/ ./notebook-include
 cp -r ./graphs/ ./notebook-include
 cp -r ./mathematics/ ./notebook-include
@@ -7,6 +9,12 @@ cp -r ./string/ ./notebook-include
 cp gen.cpp ./notebook-include
 cp rng.cpp ./notebook-include
 
+rm ./notebook-include/graphs/EdmondKarp.cpp
+rm ./notebook-include/graphs/tree-diameter.cpp
+rm ./notebook-include/string/hashing.cpp
+rm ./notebook-include/string/bin-trie.cpp
+
 cd notebook-include
-notebook-generator -c 3 -a "chungdinh, distiled, 5onyy" .
+notebook-generator -c 2 -a "chungdinh, distiled, 5onyy" .
+# mv notebook.pdf ..
 cd ..
